d3af242efa5c2a15cf713a2938edd89aed78fff3
AlexisDerumigny <AlexisDerumigny@users.noreply.github.com>
2021-10-27 11:46:40 +0200

    EllCopSim: new ways of generating the squared radius

--- R/EllCopSim.R
+++ R/EllCopSim.R
@@ -8,6 +8,19 @@
 #' @param g_d vector of values of the density generator on the `grid`.
 #' @param A square-root of the correlation matrix of X.
 #'
+#' @param genR additional arguments for the generation of the squared radius.
+#' It must be a list with a component method: \itemize{
+#'   \item If `genR$method == "pinv"`, the radius is generated
+#'   using the function [Runuran::pinv.new()].
+# Not developped yet, maybe later:
+#   \item If `genR$method == "invgrid"`, the user needs also to supply a `grid`
+#   as an additional component of `genR` and the generation of the squared radius
+#   will be done by inverting the approximation of the CDF on this grid.
+#'   \item If `genR$method == "MH"`,
+#'   the generation is done using the Metropolis-Hasting algorithm,
+#'   with a N(0,1) move at each step.
+#' }
+#'
 #' @return a matrix of size `(n,d)` with `n` observations
 #' of the `d`-dimensional elliptical copula.
 #'
@@ -20,11 +33,13 @@
 #' # Simulation from a Gaussian copula
 #' grid = seq(0,5,by = 0.01)
 #' X = EllCopSim(n = 100, d = 2, grid = grid, g_d = exp(-grid/2))
+#' X = EllCopSim(n = 100, d = 2, grid = grid, g_d = exp(-grid/2),
+#'               genR = list(method = "MH", niter = 500) )
 #' plot(X)
 #'
 #' @export
 #'
-EllCopSim <- function(n, d, grid, g_d, A = diag(d))
+EllCopSim <- function(n, d, grid, g_d, A = diag(d), genR = list(method = "pinv"))
 {
   # Probability density function of R^2
   fR2 <- Convert_gd_To_fR2(g_d = stats::approxfun(grid, g_d, yleft = 0, yright = 0), d = d)
@@ -36,7 +51,7 @@ EllCopSim <- function(n, d, grid, g_d, A = diag(d))
 
   # Sampling from the associated elliptical distribution
   ellDistrVectors = EllDistrSim(n = n, d = d, A = A, mu = 0,
-                                density_R2 = fR2)
+                                density_R2 = fR2, genR = genR)
 
   ellCopVectors = matrix(nrow = n, ncol = d)
   for (j in 1:d)
